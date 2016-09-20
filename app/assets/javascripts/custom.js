762009e1f61632c0520d7e7821b757bdc918462a
linklee <nevernight721@gmail.com>
2016-09-20 21:49:01 +0500

    new answers works and a test for it

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,16 @@
+//on turbolinks load
+var ready;
+var showAnswerForm = function () {
+	$('.new-answer-form').removeClass('hidden');
+	$(this).hide();
+	return false;
+}
+ready = function() {
+	//show new answer form on click
+	console.log('turbolinks loaded')
+	$("body").on("click", ".show-answer-link",showAnswerForm);
+};
+
+// //on on turbolinks load call ready function (it is calling just once)
+
+$(document).on('turbolinks:load', ready);
